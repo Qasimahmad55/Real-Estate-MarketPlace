a0acc9d9da859e3687c2843b37ac4523505533f7
Qasim Ahmad <qasimahmad2555@gmail.com>
2025-10-14 07:52:24 +0500

    added pages and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SiginUp'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-  
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='/about' element={<About />} ></Route>
+          <Route path='/profile' element={<Profile />} ></Route>
+          <Route path='/sign-in' element={<SignIn />} ></Route>
+          <Route path='/sign-out' element={<SignUp />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
