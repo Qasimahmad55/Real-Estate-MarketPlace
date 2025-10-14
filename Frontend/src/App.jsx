c9576c4751fc5c2d18ee6b849d3f3a4d3fc78a5a
Qasim Ahmad <qasimahmad2555@gmail.com>
2025-10-14 08:34:44 +0500

    added frontend folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SiginUp'
+import Header from './components/Header'
 function App() {
 
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} ></Route>
           <Route path='/about' element={<About />} ></Route>
