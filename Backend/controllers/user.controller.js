346e654c4963ba749a318e397ad14793f621661c
qasim ahmad <qasimahmad2555@gmail.com>
2025-11-06 21:59:33 +0500

    feat: add delete user functionality with authorization check

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -35,5 +35,16 @@ const updateUser = async (req, res, next) => {
         next(error)
     }
 }
-
-export { testRoute, updateUser }
+const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You can only delete your own account"))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie("access_token")
+        res.status(200).json("User has been deleted successfully")
+    } catch (error) {
+        next(error)
+    }
+}
+export { testRoute, updateUser, deleteUser }
