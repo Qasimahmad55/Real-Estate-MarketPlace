f4246887166aed361b157febbe3fc3afcfd6829e
qasim ahmad <qasimahmad2555@gmail.com>
2025-11-06 22:00:01 +0500

    feat: implement delete user functionality with state management in Profile component

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { Router } from "express"
-import { testRoute, updateUser, } from "../controllers/user.controller.js"
+import { testRoute, updateUser, deleteUser } from "../controllers/user.controller.js"
 import { verifyUser } from "../utils/verifyToken.js"
 const router = Router()
 
 router.get("/test", testRoute)
 router.post("/update/:id", verifyUser, updateUser)
+router.delete("/delete/:id", verifyUser, deleteUser)
 export default router
