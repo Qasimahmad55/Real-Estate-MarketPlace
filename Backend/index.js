6baaff72d87442abb1ee2c92febf6738a708cbed
qasim ahmad <qasimahmad2555@gmail.com>
2025-11-19 15:46:44 +0500

    feat: add path module import; configure static file serving and fallback for index.html in express app

--- Backend/index.js
+++ Backend/index.js
@@ -6,18 +6,28 @@ import { connectDb } from './connectDb.js'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
+import path from 'path'
 dotenv.config(
     {
         path: "./.env"
     }
 )
 connectDb()
+const __dirname = path.resolve()
+
+
 
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, '/Backend/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Backend', 'dist', 'index.html'))
+})
+
 app.listen(3000, () => {
     console.log("Server is running at 3000");
 })
