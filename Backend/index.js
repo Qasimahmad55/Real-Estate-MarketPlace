779e6dde8591ad666660394da70f11079ce8c8ff
qasim ahmad <qasimahmad2555@gmail.com>
2025-11-21 18:27:17 +0500

    fix: update route handling to correctly match all paths for static file serving

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
 
 app.use(express.static(path.join(__dirname, '/Backend/dist')))
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'Backend', 'dist', 'index.html'))
 })
 
