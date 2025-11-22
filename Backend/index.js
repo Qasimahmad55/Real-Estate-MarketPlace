50929511feee20b372dc7552a37aa70c83547f9b
qasim ahmad <qasimahmad2555@gmail.com>
2025-11-22 23:07:18 +0500

    fix: restrict CORS to specific origin for enhanced security

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ import cors from 'cors'
 // import path from 'path'
 app.use(cors(
     {
-        origin: '*'
+        origin: 'https://real-estate-market-place-ipq7.vercel.app'
     }
 ))
 dotenv.config(
@@ -21,11 +21,11 @@ dotenv.config(
 connectDb()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
-
 
 
 app.listen(3000 || process.env.PORT, () => {
